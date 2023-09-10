0b48808d5fd00125113db1cd60e95e021380152b
Taehwan2 <97010824+Taehwan2@users.noreply.github.com>
2023-09-11 08:59:11 +0900

    Time: 64 ms (5.24%), Space: 13.4 MB (25.56%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def gcdOfStrings(self,str1, str2):
+    
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        for i in range(max(len(str1), len(str2)), 0, -1):
+            print(len(str1)%i,i,len(str1))
+            if len(str1) % i == 0 and len(str2) % i == 0:
+                return max(str1, str2)[:i]
+        
