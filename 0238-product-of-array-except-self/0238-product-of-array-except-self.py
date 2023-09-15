e392745bf8bc94f43b33e5e7a351f1c0a6caa8e5
Taehwan2 <97010824+Taehwan2@users.noreply.github.com>
2023-09-15 10:53:35 +0900

    Update 0238-product-of-array-except-self.py

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,5 +1,6 @@
 class Solution(object):
     def productExceptSelf(self, nums):
+          #인덱스자리별로 자신을 제외한 왼쪽값의 합과 오른쪽 값의 합을 구하면 된다.
         output = []
         temp = 1 
         for i in range(len(nums)): 
